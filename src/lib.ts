dff92e2e048d55b23d3d43b7c1fb10b19f4f78be
instajs <ycwong@instajs.com>
2022-07-04 12:20:50 +0800

    Update lib.ts

--- src/lib.ts
+++ src/lib.ts
@@ -1,6 +1,6 @@
 export default `
-    /// <reference no-default-lib="true"/>
-    declare var $JSX;
+/// <reference no-default-lib="true"/>
+declare var $JSX;
     /*! *****************************************************************************
 Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use
