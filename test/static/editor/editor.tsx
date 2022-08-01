11a10f62ac2028e33bc5996a87cbfdb6de1409bb
mike-yuen <mike.dev@instajs.com>
2022-08-01 17:43:18 +0700

    update editor style

--- test/static/editor/editor.tsx
+++ test/static/editor/editor.tsx
@@ -472,7 +472,6 @@ export default class CodeEditorModule extends Module {
                 >
                   <i-tree-view
                     id="tvFiles"
-                    dock="fill"
                     onClick={this.handleTreeViewClick}
                     onDblClick={this.handleTreeViewDblClick}
                   ></i-tree-view>
--- types/lib.d.ts
+++
@@ -1,2 +0,0 @@
-declare const Lib: string;
-export default Lib;
